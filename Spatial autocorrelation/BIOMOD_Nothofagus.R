ee2e31eb3d189feff3ba4a9ef08bb077ab3fa83e
tufui57 <nomura_jpngm@live.jp>
2019-10-31 18:02:27 +1300

    Update BIOMOD_Nothofagus.R

--- Spatial autocorrelation/BIOMOD_Nothofagus.R
+++ Spatial autocorrelation/BIOMOD_Nothofagus.R
@@ -38,41 +38,30 @@ source(".\\Range-fillings\\F_Create_Package_BIOMOD.r")
 tempFilePath <- list.files(rasterOptions()$tmpdir, full.names = T)
 file.remove(tempFilePath)
 
-##################################################################################################################
-########################        1km grid data import
-##################################################################################################################
-# character string of target genus name
-genus_name <- "Acaena"
-# genus_name <- "Chionochloa"
+genus_name = "Nothofagus"
 
-### Set arguments
-# data frame of occurrence data and climate data
-if (genus_name == "Acaena"){
-  datapath <- "Y://1st chapter_Acaena project//Acaena manuscript//meta data//Acaena_bioclim_landcover_history_worldclim1_1km.csv"
-}else{
-  datapath <- "Y://Chionochloa_bioclim_landcover_history_worldclim1_1km.csv"
-}
-
+##################################################################################################################
+########################        5 km grid data import
+##################################################################################################################
 
 ### Import raster of climate data
 # Reference raster for coordinate system. This raster must have the same dimentions as the raster of occurrence data
 ref.raster <- raster(
-  paste("Y://GIS map and Climate data//newzealandpotentialvegetatio1.bil", sep="")
+  paste("Y://GIS map and Climate data//newzealandpotentialvegetatio5.bil", sep="")
 )
 proj4stringNZTM <- proj4string(ref.raster)
 # Worldclim ver.1.4
 path <-"Y:\\GIS map and Climate data\\worldclim\\bio_411"
-source(".\\GitHub\\functions\\F01_project_resample_WORLDCLIM.R")
+source(".\\functions\\F01_project_resample_WORLDCLIM.R")
 
 
 # Import data frame of bioclim and occurrence records
-climate.occ <- read.csv(datapath)
+climate.occ <- read.csv("Y://Nothofagus_in_nz.csv")
 # Change colmun name of coordinates 
 names(climate.occ)[names(climate.occ) %in% c("x","y")] <- c("NZTMlon", "NZTMlat")
 
 # Change object name of bioclim rasters
 data.ras <- bioNZ
-
 
 # Get species name
 spname <- colnames(climate.occ)[grepl(genus_name, colnames(climate.occ))]
@@ -81,15 +70,9 @@ spname <- colnames(climate.occ)[grepl(genus_name, colnames(climate.occ))]
 climate.occ2 <- (is.na(climate.occ$bioclim1) == F) %>% climate.occ[.,]
 # Remove rows which have no occurrence records of any species
 climate.occ3 <- (rowSums(climate.occ2[, spname], na.rm = T) > 0) %>% climate.occ2[.,]
-# Remove species with < 10 presence records out of species name vector.
-spname <- spname[(climate.occ3[, spname] %>% colSums(., na.rm = TRUE)) > 9]
-# Remove wrong species names
-if (genus_name == "Acaena"){
-  spname <-  spname[spname!="Acaena_microphylla"] 
-}
 
 ### Generate pseudo-absence
-### I assume cells with any other species than the target species as the cells where the target species was absent. 
+### I assume cells with any other species than the target species as the cells where the target species was absent.
 for(i in spname){climate.occ3[is.na(climate.occ3[, i]), i] <- 0}
 
 # Environmental variables extracted from BIOCLIM and converted into NZTM.
@@ -99,7 +82,9 @@ myExpl <- stack(data.ras[c("bioclim1", "bioclim6", "bioclim12", "bioclim15")])
 if(dir.exists("Y://BIOMOD for Grid2") == FALSE){
   dir.create("Y://BIOMOD for Grid2")
 }
+
 setwd("Y://BIOMOD for Grid2")
+
 #########################################################
 ## Takes a while to run... Don't run on laptop! Slow!
 #########################################################
@@ -109,8 +94,8 @@ setwd("Y://BIOMOD for Grid2")
 
 for(i in spname){
   tryCatch(
-    runBiomod(i, data = climate.occ3, myExpl = myExpl, folder.name = "7Nov18"),
-    error=function(e){cat("ERROR :",conditionMessage(e), "\n")}
+    runBiomod(i, data = climate.occ3, myExpl = myExpl, folder.name = "31Oct19"),
+    error = function(e){cat("ERROR :", conditionMessage(e), "\n")}
     
   )
 }
